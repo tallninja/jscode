915a1fb914729de36e3ba6d54b822bf4f8e66305
Ernest Wambua <ernestwambua2@gmail.com>
2022-05-22 14:02:35 +0300

    Escape CSS Snippets

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -20,22 +20,36 @@ export const fetchPlugin = (userInput: string) => {
 				}
 
 				// check to see if file is in cache
-				const chachedFile = await fileCache.getItem<esbuild.OnLoadResult>(
-					args.path
-				);
+				// const chachedFile = await fileCache.getItem<esbuild.OnLoadResult>(
+				// 	args.path
+				// );
 
-				// if it is return the file from the cache
-				if (chachedFile) {
-					console.log('Fetched from cache!');
-					return chachedFile;
-				}
+				// // if it is return the file from the cache
+				// if (chachedFile) {
+				// 	console.log('Fetched from cache!');
+				// 	return chachedFile;
+				// }
 				// if not fetch the file from unpkg and return it
 				const { data, request } = await axios.get(args.path); // contains the responseURL where unpkg redirected us to
 
-				const loader = args.path.match(/.css$/) ? 'css' : 'jsx';
+				const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+
+				const escapedCSS = data
+					.replace(/\n/g, '')
+					.replace(/"/g, '\\"')
+					.replace(/'/g, "\\'");
+
+				const contents =
+					fileType === 'css'
+						? `
+                const style = document.createElement('style');
+                style.innerText = '${escapedCSS}';
+                document.head.appendChild(style);
+                `
+						: data;
 				const fetchedFile: esbuild.OnLoadResult = {
-					loader: loader,
-					contents: data,
+					loader: 'jsx',
+					contents: contents,
 					resolveDir: new URL('./', request.responseURL).pathname,
 				};
 
