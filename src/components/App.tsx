6c8f0330c589084bff87d78a1b680f2a3c353359
Ernest Wambua <ernestwambua2@gmail.com>
2022-05-22 14:24:41 +0300

    Using unpkg to load the wasm file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,7 +15,7 @@ const App: React.FC = () => {
 	const startService = async () => {
 		serviceRef.current = await esbuild.startService({
 			worker: true,
-			wasmURL: '/esbuild.wasm',
+			wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
 		});
 	};
 
